88ba920d6482687f7c6d0f4f779dca3da23a6ae4
DoctorChe <duncan.reg@yandex.ru>
2020-03-17 16:58:38 +0300

    change bootstrap version change view for main page

--- companystatistics/main_app/views.py
+++ companystatistics/main_app/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Company Statistics App')
+    return render(request, 'main_app/index.html', {})
